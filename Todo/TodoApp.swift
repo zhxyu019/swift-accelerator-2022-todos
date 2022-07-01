137f9a5c87bbf6ec9a26002944ce6d7f2ee36534
Zhiyu <zhiyuma2006@gmail.com>
2022-07-02 00:16:02 +0800

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by TinkerTanker on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
